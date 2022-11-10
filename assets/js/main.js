574ce8a6230cd759d6694d0081b70b4ce38a2875
Mateusz Mróz <mateuszmroz001@gmail.com>
2022-11-10 22:07:50 +0100

    test body flex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-        // We execute the same script as before
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-
-
-
     const $body = $('body');
     const $header = $('.header');
     const $navbar = $('.navbar');
@@ -407,6 +392,14 @@ $(document).ready(function () {
         }
         animate();
     });
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    const appHeight = () => {
+        const doc = document.documentElement
+        doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight()
+
 });
 
 
